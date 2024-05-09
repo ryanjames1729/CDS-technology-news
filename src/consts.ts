daeddb36194c3e98b9dd2d2721917e3948f7e26c
Ryan James <Ryan James>
2024-05-09 10:38:58 -0400

    initial setup

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'CDS Technology News';
+export const SITE_DESCRIPTION = 'The latest news and updates from the CDS Technology team.';
